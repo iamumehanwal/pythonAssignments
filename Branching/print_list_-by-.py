0a2e2123eb4100c126a010aac384b1915a521940
utkrishtha <utkrishthamehanwal@gmail.com>
2019-11-08 00:01:47 +0530

    Printed a list

---
+++ Branching/print_list_-by-.py
@@ -0,0 +1,10 @@
+product_companies = ['Apple', 'Google', 'Samsung', 'LG', 'Moto', 'Sony']              # List 
+no_comp = len(product_companies)                                                      # Count of the list
+
+product_companies.sort()                                                              # Sorting the list
+
+i=0                                                                                   # Variable used for while loop
+while i < no_comp:
+    print(str(i+1) + ' : ' + product_companies[i])
+    i+=1
+
