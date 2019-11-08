07b5deb94051c20b632b9aaf0a638a8c822b71e2
utkrishtha <utkrishthamehanwal@gmail.com>
2019-11-08 06:52:10 +0530

    Completed

---
+++ Branching/dictionary_exc.py
@@ -0,0 +1,17 @@
+iphone = {
+        'model_name' : 'iphone 11',
+        'colors_available' : ['Black', 'green', 'Yellow', 'Purple', 'Product Red', 'White'],
+        'capacity' : [64, 128, 256],
+        'display' : 'Liquid Retina HD display with 6.1‑inch (diagonal) all-screen LCD',
+        'screen_resolution' : '1792x828‑pixel resolution at 326 ppi',
+        'water_resistant' : 'Rated IP68',
+        'chip' : 'A13 Bionic chip - Third‑generation Neural Engine'
+}
+
+print(iphone['model_name'])
+print(iphone['colors_available'])
+print(iphone['capacity'])
+print(iphone['display'])
+print(iphone['screen_resolution'])
+print(iphone['water_resistant'])
+print(iphone['chip'])
--- Branching/print_list_-by-.py
+++ Branching/print_list_-by-.py
@@ -1,7 +1,7 @@
 product_companies = ['Apple', 'Google', 'Samsung', 'LG', 'Moto', 'Sony']              # List 
 no_comp = len(product_companies)                                                      # Count of the list
 
-product_companies.sort()                                                              # Sorting the list
+#product_companies.sort()                                                              # Sorting the list
 
 i=0                                                                                   # Variable used for while loop
 while i < no_comp:
